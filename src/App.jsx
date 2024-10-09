40e6d4e53b59950d189a3fbcdf8f3d70ce469da8
Pranay2638 <pranaysanjayv@gmail.com>
2024-10-09 14:37:02 +0530

    skill added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Components/Navbar'
+
 import Footer from './Components/Footer'
 import Main from "./Components/Main"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
     <Main/>
     <Footer/>
     </>
