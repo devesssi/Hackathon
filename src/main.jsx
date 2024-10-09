eb7608e8d82dc9086d9bef3d5913ade63c82003c
DEVESSI <tiwaridevesh745@gmail.com>
2024-10-09 16:11:10 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,17 @@ import Layout from './Components/Layout'; // Ensure Layout is defined
 import CoursesPage from './Components/Features/CourcesPage';
 import SkillsPage from './Components/Features/Skill';
 import HomePage from './Components/Features/HomePage'
+import Vlab from './Components/Features/vlab';
 
 // Creating the router using react-router-dom
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-<<<<<<< HEA
-      
-      
-      
-=======
     <Route path="/courses" element={<CoursesPage/>} />
     <Route path="/features" element={<SkillsPage />} />
     <Route path="/" element={<HomePage />} />
->>>>>>> aafaa42afc8c8b4a91e560c98b9a785cde443d08
+    <Route path="/vlab" element={<Vlab />} />
+
     </Route>
   )
 );
