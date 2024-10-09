13c0fbbe7cd3c414b62a63eb30e0050d98172ce6
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-10-09 11:55:18 +0530

    config the tailwing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Main from "./Components/Main"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hackathon
+    <Navbar/>
+    <Main/>
+    <Footer/>
     </>
   )
 }
